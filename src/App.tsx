ec4d7b074e6dfb8ded516c67ae7baba03901cf2d
alexkarunny <karunny.alex@gmail.com>
2021-03-31 10:19:16 +0300

    Lesson 7

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,35 @@ import React from 'react';
 
 
 function App() {
-  return (
-    <div className="App">
+    return (
+        <div className="App">
+            <Header/>
+            <Tech/>
+        </div>
+    );
+}
+
+function Header() {
+    return (
+        <div>
+            <a href="#1">a</a>
+            <a href="#1">b</a>
+            <a href="#1">c</a>
+        </div>
+    )
+}
+
+function Tech() {
+    return (
         <ul>
             <li>css</li>
             <li>saa</li>
             <li>asf</li>
             <li>fsa</li>
         </ul>
-    </div>
-  );
+    )
 }
 
 export default App;
+
+

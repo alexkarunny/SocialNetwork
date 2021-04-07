f9bbf3dfbacd284e55fb54785bf3cb04bc202546
Alex Karunny <karunny.alex@gmail.com>
2021-04-07 12:02:46 +0300

    Lesson 18

---
+++ src/Components/Dialogs/Dialogs.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import classes from './Dialogs.module.css'
+
+type DialogsPropsType = {
+
+}
+
+export function Dialogs(props: DialogsPropsType) {
+    return (
+        <div>
+            Dialogs
+        </div>
+    )
+}

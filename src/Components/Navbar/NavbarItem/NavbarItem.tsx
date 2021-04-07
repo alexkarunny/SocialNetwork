52b4fb1ec18fb05ed8bfaf94579b44fd8b388373
Alex Karunny <karunny.alex@gmail.com>
2021-04-07 16:06:26 +0300

    Lesson 20

--- src/Components/Navbar/NavbarItem/NavbarItem.tsx
+++ src/Components/Navbar/NavbarItem/NavbarItem.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import {NavLink} from "react-router-dom";
 import classes from './NavbarItem.module.css'
 
 
@@ -10,7 +11,7 @@ type NavbarItemPropsType = {
 export function NavbarItem(props: NavbarItemPropsType) {
     return (
         <div className={`${classes.item} ${classes.active}`}>
-            <a href={props.link}>{props.title}</a>
+            <NavLink to={props.link} activeClassName={classes.activeLink}>{props.title}</NavLink>
         </div>
 
     )
--- src/Components/Profile/Profile.tsx
+++ src/Components/Profile/Profile.tsx
@@ -3,15 +3,17 @@ import classes from './Profile.module.css'
 import {MyPosts} from "./MyPosts/MyPosts";
 
 export function Profile() {
-    return(
+    return (
         <div className={classes.content}>
             <div>
-                <img src="https://static8.depositphotos.com/1370441/848/i/600/depositphotos_8486144-stock-photo-beach-and-tropical-sea.jpg" alt="Cat"/>
+                <img
+                    src="https://static8.depositphotos.com/1370441/848/i/600/depositphotos_8486144-stock-photo-beach-and-tropical-sea.jpg"
+                    alt="Cat"/>
             </div>
             <div>
                 ava + description
             </div>
-            <MyPosts />
+            <MyPosts/>
 
         </div>
     )

b3b376d55a7b7446960a3e0d9e2a67245d3cb605
Alex Karunny <karunny.alex@gmail.com>
2021-04-14 17:10:53 +0300

    Lesson 24

--- src/Components/Dialogs/Dialogs.tsx
+++ src/Components/Dialogs/Dialogs.tsx
@@ -10,22 +10,39 @@ type DialogsPropsType = {
     titleMessage: string
 }
 
+type DialogsDataPropsData = {
+    id: string
+    name: string
+}
+
+type MessagesDataPropsType = {
+    id: number
+    message: string
+}
+
 export function Dialogs(props: DialogsPropsType) {
+
+    let dialogsData: Array<DialogsDataPropsData> = [
+        {id: '1', name: 'Alex'},
+        {id: '2', name: 'Alexei'}
+    ]
+
+    let messagesData: Array<MessagesDataPropsType> = [
+        {id: 1, message: 'Hello'},
+        {id: 2, message: 'I\'m fine. just not happy'}
+    ]
+
     return (
         <div className={classes.title}>
             <div className={classes.dialogs}>
                 <h3>{props.titleDialog}</h3>
-                <Dialog dialogName={'Alex'} id={'1'}/>
-                <Dialog dialogName={'Sveta'} id={'2'}/>
-                <Dialog dialogName={'Alexei'} id={'3'}/>
-                <Dialog dialogName={'Viktor'} id={'4'}/>
-
+                <Dialog dialogName={dialogsData[0].name} id={dialogsData[0].id}/>
+                <Dialog dialogName={dialogsData[1].name} id={dialogsData[1].id}/>
             </div>
             <div className={classes.messages}>
                 <h3>{props.titleMessage}</h3>
-                <Message messageText={'Hello'}/>
-                <Message messageText={'How are you'}/>
-                <Message messageText={'I\'m fine. just not happy'}/>
+                <Message messageText={messagesData[0].message}/>
+                <Message messageText={messagesData[1].message}/>
             </div>
         </div>
     )
--- src/Components/Profile/MyPosts/MyPosts.tsx
+++ src/Components/Profile/MyPosts/MyPosts.tsx
@@ -2,7 +2,19 @@ import React from "react";
 import classes from './MyPosts.module.css'
 import {Post} from "./Post/Post";
 
+type PostDataPropsType = {
+    id: number
+    message: string
+    likes: number
+}
+
 export function MyPosts() {
+
+    let postData: Array<PostDataPropsType> = [
+        {id: 1, message: 'Hi, how are you', likes: 10},
+        {id: 2, message: 'It\'s my first post', likes: 15},
+    ]
+
     return (
         <div className={classes.myPostsBlock}>
             <h3>My posts</h3>
@@ -14,8 +26,8 @@ export function MyPosts() {
                 </div>
             </div>
             <div className={classes.posts}>
-                <Post message={"Hi, how are you"} likes={10}/>
-                <Post message={"It's my first post"} likes={15}/>
+                <Post message={postData[0].message} likes={postData[0].likes}/>
+                <Post message={postData[1].message} likes={postData[1].likes}/>
             </div>
         </div>
     )

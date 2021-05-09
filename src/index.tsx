5cc8ed5d1aa6300e004bd8cc46fdc4f2e7e54cf8
alex <karunny.alex@gmail.com>
2021-05-09 15:23:15 +0300

    lesson 37

--- src/index.tsx
+++ src/index.tsx
@@ -1,29 +1,31 @@
 import React from 'react';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import {addNewPostMessage, addPost, RootStatePropsType, state, subscribe} from "./redux/state";
+import {store} from "./redux/state";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-export const renderTree = (state: RootStatePropsType) => {
+let state = store.getState()
+
+export const renderTree = () => {
     ReactDOM.render(
         <React.StrictMode>
             <App dialogs={state.dialogsPage.dialogs}
                  linkItems={state.navbarPage.linkItems}
                  messages={state.dialogsPage.messages}
                  posts={state.profilePage.posts}
-                 addPost={addPost}
+                 addPost={store.addPost.bind(store)}
                  newPostMessage={state.profilePage.newPostMessage}
-                 addNewPostMessage={addNewPostMessage}
+                 addNewPostMessage={store.addNewPostMessage.bind(store)}
             />
         </React.StrictMode>,
         document.getElementById('root')
     );
 }
 
-renderTree(state);
+renderTree();
 
-subscribe(renderTree)
+store.subscribe(renderTree)
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
--- src/redux/state.ts
+++ src/redux/state.ts
@@ -1,6 +1,70 @@
 import {v1} from "uuid";
-let renderTree = (state: RootStatePropsType) => {
-    console.log(state);
+
+export type StoreType = {
+    _state: RootStatePropsType
+    addPost: (postMessage: string) => void
+    addNewPostMessage: (newPostText: string) => void
+    _onChange: () => void
+    subscribe: (observer: () => void) => void
+    getState: () => RootStatePropsType
+}
+
+export const store: StoreType  = {
+    _state: {
+        profilePage: {
+            newPostMessage:'',
+            posts: [
+                {id: v1(), message: 'Hi, how are you', likes: 10},
+                {id: v1(), message: 'It\'s my first post', likes: 15},
+                {id: v1(), message: 'It\'s my second post', likes: 5},
+            ]
+        },
+        dialogsPage: {
+            messages: [
+                {id: v1(), message: 'Hello'},
+                {id: v1(), message: 'I\'m fine. just not happy'},
+                {id: v1(), message: 'I\'m fine.'}
+            ],
+            dialogs: [
+                {id: '1', name: 'Alex'},
+                {id: '2', name: 'Alexei'},
+                {id: '3', name: 'Ivan'},
+                {id: '4', name: 'Katya'},
+                {id: '5', name: 'Jenya'},
+                {id: '6', name: 'Andrew'},
+
+            ]
+        },
+        navbarPage: {
+            linkItems: [
+                {title: 'Profile', link: '/profile'},
+                {title: 'Dialog', link: '/dialogs'},
+                {title: 'News', link: '/news'},
+                {title: 'Music', link: '/music'},
+                {title: 'Settings', link: '/settings'},
+            ]
+        }
+    },
+    addPost(postMessage){
+        const newPost: PostsPropsType = {id: v1(), message: postMessage, likes: 0 }
+        this._state.profilePage.posts.push(newPost)
+        this._state.profilePage.newPostMessage = ''
+        this._onChange();
+    },
+    addNewPostMessage(newPostText: string) {
+        this._state.profilePage.newPostMessage = newPostText
+        this._onChange()
+    },
+    _onChange(){
+        console.log('state changed');
+    },
+    subscribe(observer) {
+        this._onChange = observer
+    },
+    getState() {
+        return this._state
+    }
+
 }
 
 export type DialogPropsType = {
@@ -44,57 +108,5 @@ export type RootStatePropsType = {
     navbarPage: NavbarPagePropsType
 }
 
-export const state: RootStatePropsType = {
-    profilePage: {
-        newPostMessage:'',
-        posts: [
-            {id: v1(), message: 'Hi, how are you', likes: 10},
-            {id: v1(), message: 'It\'s my first post', likes: 15},
-            {id: v1(), message: 'It\'s my second post', likes: 5},
-        ]
-    },
-    dialogsPage: {
-        messages: [
-            {id: v1(), message: 'Hello'},
-            {id: v1(), message: 'I\'m fine. just not happy'},
-            {id: v1(), message: 'I\'m fine.'}
-        ],
-        dialogs: [
-            {id: '1', name: 'Alex'},
-            {id: '2', name: 'Alexei'},
-            {id: '3', name: 'Ivan'},
-            {id: '4', name: 'Katya'},
-            {id: '5', name: 'Jenya'},
-            {id: '6', name: 'Andrew'},
 
-        ]
-    },
-    navbarPage: {
-        linkItems: [
-            {title: 'Profile', link: '/profile'},
-            {title: 'Dialog', link: '/dialogs'},
-            {title: 'News', link: '/news'},
-            {title: 'Music', link: '/music'},
-            {title: 'Settings', link: '/settings'},
-        ]
-    }
-}
-
-export const addPost = (postMessage: string) => {
-    const newPost: PostsPropsType = {id: v1(), message: postMessage, likes: 0 }
-    state.profilePage.posts.push(newPost)
-    debugger
-    state.profilePage.newPostMessage = ''
-    renderTree(state);
-
-}
-
-export const addNewPostMessage = (newPostText: string) => {
-    state.profilePage.newPostMessage = newPostText
-    renderTree(state)
-}
-
-export const subscribe = (observer: (state: RootStatePropsType) => void) => {
-    renderTree = observer
-}
 
